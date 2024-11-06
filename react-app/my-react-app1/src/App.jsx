6b0defcfaca11ac0c3fe18b1eddb6019f71e809c
yulin1122 <uilin180821@gmail.com>
2024-11-06 11:50:15 +0800

    1106

---
+++ react-app/my-react-app1/src/App.jsx
@@ -0,0 +1,23 @@
+
+// 建立新的元件
+function MyComponent() {
+  return
+  // 空標籤 : Fragment
+  <>
+    <h1>React</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci facilis odit minima eius voluptatum voluptates ullam sit enim quis dolorum? Natus sequi rem dolorum ad inventore dolor ut placeat animi.</p>
+  </>
+}
+
+function App() {
+  return (
+    <div>
+
+      <MyComponent />
+
+    </div>
+
+  )
+}
+
+export default App
